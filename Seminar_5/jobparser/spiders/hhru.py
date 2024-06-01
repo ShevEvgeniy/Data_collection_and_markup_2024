acfdce1e54a8c4d87877c1f72071570bf152a3e5
ShevEvgeniy <shivev@gmail.com>
2024-06-01 22:29:16 +0300

    Seminar 5

---
+++ Seminar_5/jobparser/spiders/hhru.py
@@ -0,0 +1,26 @@
+import scrapy
+from scrapy.http import HtmlResponse
+from jobparser.items import JobparserItem
+
+class HhruSpider(scrapy.Spider):
+    name = "hhru"
+    allowed_domains = ["hh.ru"]
+    start_urls = ["https://nn.hh.ru/search/vacancy?ored_clusters=true&hhtmFrom=vacancy_search_list&hhtmFromLabel=vacancy_search_line&search_field=name&search_field=company_name&search_field=description&text=python&enable_snippets=false&L_save_area=true"]
+
+    def parse(self, response:HtmlResponse):    
+        next_page = response.xpath("//a[@data-qa='pager-next']/@href").get()
+        if next_page:
+            yield response.follow(next_page, callback=self.parse)
+       
+        links = response.xpath("//a[data-qa='serp-item__title']/href").getall()
+        for link in links:
+            yield response.follow(link, callback=self.vacancy_parse)
+
+        
+
+    def vacancy_parse(self, response:HtmlResponse):
+        name=response.xpath("//h1/text()").get()
+        salary = response.xpath("//div[@data-qa='vacancy-salary']//text()").getall()
+        url = response.url
+        yield JobparserItem(name=name, salary=salary, url=url)
+        
